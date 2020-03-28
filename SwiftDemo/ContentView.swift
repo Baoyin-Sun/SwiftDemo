3bd77d50c501071f5063fc3624a953472b3e877b
BaoYin <997344374@qq.com>
2020-03-28 22:00:37 +0800

    first commit

---
+++ SwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftDemo
+//
+//  Created by 孙宝音 on 2020/3/28.
+//  Copyright © 2020 孙宝音. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
