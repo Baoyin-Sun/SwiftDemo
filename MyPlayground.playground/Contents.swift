43f17303975bb321d63a51c2c9829dff3b8d4f74
BaoYin <997344374@qq.com>
2020-03-30 23:38:37 +0800

    类

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -212,4 +212,28 @@ let sortedNumbers = numbers.sorted { $0 > $1 }
 print(sortedNumbers)
 
 
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
 
+
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
+
+
+class NamedShape {
+    var numberOfSides: Int = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
